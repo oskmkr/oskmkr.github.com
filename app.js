d0a203ef5f7cc520fb50e78e83bef037df3ba2ff
oskmkr <oskmkr@naver.com>
2014-05-09 20:08:26 +0900

    simple nodejs webserver served static resources

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var http = require('http');
+var fs = require('fs');
+
+var port = 8080;
+console.log('start simple web server...', port);
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Cloud9\n');
-}).listen(process.env.PORT);
+	
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var index = fs.readFileSync('oskmBlogApp.html');
+	res.end(index);
+
+}).listen(port);
