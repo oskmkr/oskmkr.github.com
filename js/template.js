fdb37d459b182e16a28249121d177a4cdfb429be
oskmkr <oskmkr@naver.com>
2015-03-13 20:03:02 +0900

    test

--- js/template.js
+++ js/template.js
@@ -14,25 +14,6 @@
 		
 		// Fixed header
 		//-----------------------------------------------
-		$(window).scroll(function() {
-			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
-					$("body").addClass("fixed-header-on");
-				} else {
-					$("body").removeClass("fixed-header-on");
-				}
-			};
-		});
-
-		$(window).load(function() {
-			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
-					$("body").addClass("fixed-header-on");
-				} else {
-					$("body").removeClass("fixed-header-on");
-				}
-			};
-		});
 
 		//Scroll Spy
 		//-----------------------------------------------
